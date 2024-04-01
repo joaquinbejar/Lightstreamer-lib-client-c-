5b52f0f319d86c74a95a05a20fbd54d4c670022b
Joaquin Bejar <jb@taunais.com>
2024-04-01 18:38:23 +0200

    v0.1.0.alpha.4 define ChangeSubscriptionRequest

--- include/lightstreamer/client/requests/ChangeSubscriptionRequest.hpp
+++ include/lightstreamer/client/requests/ChangeSubscriptionRequest.hpp
@@ -24,4 +24,59 @@
 #ifndef LIGHTSTREAMER_LIB_CLIENT_CPP_CHANGESUBSCRIPTIONREQUEST_HPP
 #define LIGHTSTREAMER_LIB_CLIENT_CPP_CHANGESUBSCRIPTIONREQUEST_HPP
 #include <lightstreamer/client/requests/ControlRequest.hpp>
+#include <cassert>
+#include <string>
+
+namespace lightstreamer::client::requests {
+
+    /**
+     * Represents a request to change subscription parameters.
+     */
+    class ChangeSubscriptionRequest : public ControlRequest {
+    private:
+        int reconfId;
+        int subscriptionId;
+
+    public:
+        /**
+         * Constructs a ChangeSubscriptionRequest with specified parameters.
+         *
+         * @param subscriptionId The ID of the subscription to be changed.
+         * @param requestedMaxFrequency The requested maximum update frequency for the subscription.
+         * @param reconfId The reconfiguration identifier.
+         */
+        ChangeSubscriptionRequest(int subscriptionId, double requestedMaxFrequency, int reconfId)
+                : reconfId(reconfId), subscriptionId(subscriptionId) {
+            this->addParameter("LS_op", "reconf");
+            this->addParameter("LS_subId", std::to_string(subscriptionId));
+
+            assert(requestedMaxFrequency != -2);
+            assert(requestedMaxFrequency != -1);
+            if (requestedMaxFrequency == 0) {
+                this->addParameter("LS_requested_max_frequency", "unlimited");
+            } else if (requestedMaxFrequency > 0) {
+                this->addParameter("LS_requested_max_frequency", std::to_string(requestedMaxFrequency));
+            }
+        }
+
+        /**
+         * Gets the reconfiguration ID.
+         *
+         * @return The reconfiguration ID.
+         */
+        int getReconfId() const {
+            return reconfId;
+        }
+
+        /**
+         * Gets the subscription ID.
+         *
+         * @return The subscription ID.
+         */
+        int getSubscriptionId() const {
+            return subscriptionId;
+        }
+    };
+}
+
 #endif //LIGHTSTREAMER_LIB_CLIENT_CPP_CHANGESUBSCRIPTIONREQUEST_HPP

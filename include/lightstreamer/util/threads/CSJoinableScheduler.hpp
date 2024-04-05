324f7a26593bbb95b1e4bfe2949170f370df25ce
Joaquin Bejar <jb@taunais.com>
2024-04-05 07:59:28 +0200

    v0.1.0.alpha.4 define CSJoinableScheduler

---
+++ include/lightstreamer/util/threads/CSJoinableScheduler.hpp
@@ -0,0 +1,80 @@
+/******************************************************************************
+    Author: Joaquin Bejar Garcia 
+    Email: jb@taunais.com 
+    Date: 5/4/24
+ ******************************************************************************/
+
+/*******************************************************************************
+ Copyright (c) 2024.
+
+ This program is free software: you can redistribute it and/or modify it
+ under the terms of the GNU General Public License as published by the
+ Free Software Foundation, either version 3 of the License, or (at your
+ option) any later version.
+
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program. If not, see <https://www.gnu.org/licenses/>..
+ ******************************************************************************/
+
+#ifndef LIGHTSTREAMER_LIB_CLIENT_CPP_CSJOINABLESCHEDULER_HPP
+#define LIGHTSTREAMER_LIB_CLIENT_CPP_CSJOINABLESCHEDULER_HPP
+#include <stack>
+#include <lightstreamer/util/threads/CSJoinableScheduler.hpp>
+#include <lightstreamer/util/threads/providers/JoinableExecutor.hpp>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <map>
+#include <chrono>
+#include <future>
+
+class CSJoinableScheduler : public JoinableScheduler {
+private:
+    std::stack<std::future<void>> currentTasks;
+    std::string threadName;
+    long keepAliveTime;
+    JoinableExecutor executor;
+    std::map<std::future<void>, std::shared_ptr<std::promise<void>>> cancs;
+    std::mutex m;
+
+public:
+    CSJoinableScheduler(std::string threadName, long keepAliveTime) : threadName(threadName), keepAliveTime(keepAliveTime) {}
+
+    CSJoinableScheduler(std::string threadName, long keepAliveTime, JoinableExecutor executor) : threadName(threadName), keepAliveTime(keepAliveTime), executor(executor) {}
+
+    void join() {
+        std::lock_guard<std::mutex> lock(m);
+        for (auto &entry : cancs) {
+            if ( entry.second != nullptr ) {
+                entry.second->set_value();
+            }
+        }
+        cancs.clear();
+    }
+
+    void Dequeue(std::future<void> tsk) {
+        std::lock_guard<std::mutex> lock(m);
+        cancs.erase(tsk);
+    }
+
+    std::shared_ptr<std::promise<void>> schedule(std::function<void()> task, long delayInMillis) {
+        std::shared_ptr<std::promise<void>> source = std::make_shared<std::promise<void>>();
+        std::lock_guard<std::mutex> lock(m);
+        std::future<void> tsk_p = std::async(std::launch::async, [=]() {
+            std::this_thread::sleep_for(std::chrono::milliseconds(delayInMillis));
+            if (source->get_future().wait_for(std::chrono::seconds(0)) == std::future_status::timeout) {
+                executor.execute(task);
+            }
+        });
+        cancs[tsk_p] = source;
+        tsk_p.share().wait();
+        this->Dequeue(tsk_p);
+        return source;
+    }
+};
+#endif //LIGHTSTREAMER_LIB_CLIENT_CPP_CSJOINABLESCHEDULER_HPP

ba0e0954bb973226737fd3a08d71d22f5287b615
Joaquin Bejar <jb@taunais.com>
2024-04-15 08:02:11 +0200

    v0.1.0.alpha.4 define MDC MDCProvider Log4JMDCProvider

--- include/lightstreamer/util/mdc/Log4JMDCProvider.hpp
+++ include/lightstreamer/util/mdc/Log4JMDCProvider.hpp
@@ -23,5 +23,42 @@
 
 #ifndef LIGHTSTREAMER_LIB_CLIENT_CPP_LOG4JMDCPROVIDER_HPP
 #define LIGHTSTREAMER_LIB_CLIENT_CPP_LOG4JMDCPROVIDER_HPP
+#include <lightstreamer/util/mdc/MDCProvider.hpp>
+#include <lightstreamer/util/mdc/MDC.hpp>
+#include <string>
+
+namespace com::lightstreamer::util::mdc {
+    
+    /**
+     * @class Log4JMDCProvider
+     * @brief A MDC provider relying on a logging framework similar to Log4J's MDC mechanism.
+     *
+     * This class is an adapter over a hypothetical C++ logging framework's MDC functionality.
+     */
+    class Log4JMDCProvider : public MDCProvider {
+    public:
+        void put(const std::string& key, const std::string& value) override {
+            // Hypothetical usage of a global MDC-like storage in the logging framework
+            MCD::put(key, value);
+        }
+
+        std::string get(const std::string& key) override {
+            // Hypothetical usage of a global MDC-like storage in the logging framework
+            return MCD::get(key);
+        }
+
+        void remove(const std::string& key) override {
+            // Hypothetical usage of a global MDC-like storage in the logging framework
+            MCD::remove(key);
+        }
+
+        void clear() override {
+            // Hypothetical usage of a global MDC-like storage in the logging framework
+            MCD::clear();
+        }
+    };
+    
+}
+
 
 #endif //LIGHTSTREAMER_LIB_CLIENT_CPP_LOG4JMDCPROVIDER_HPP

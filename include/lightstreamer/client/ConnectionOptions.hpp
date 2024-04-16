bd05218ad8fa3a4b8c4fbfbc321fa4c398798665
Joaquin Bejar <jb@taunais.com>
2024-04-16 16:51:36 +0200

    v0.1.0.alpha.4 define ConnectionOptions

--- include/lightstreamer/client/ConnectionOptions.hpp
+++ include/lightstreamer/client/ConnectionOptions.hpp
@@ -23,5 +23,129 @@
 
 #ifndef LIGHTSTREAMER_LIB_CLIENT_CPP_CONNECTIONOPTIONS_HPP
 #define LIGHTSTREAMER_LIB_CLIENT_CPP_CONNECTIONOPTIONS_HPP
+#include <memory>
+#include <string>
+#include <map>
+#include <stdexcept>
+#include <mutex>
+#include <lightstreamer/client/session/InternalConnectionOptions.hpp>
+
+namespace lightstreamer::client {
+
+    /**
+     * @brief Represents a class used by LightstreamerClient to provide extra connection properties.
+     *
+     * This class acts as a proxy for the InternalConnectionOptions class to expose certain elements.
+     * An instance of this class is attached to every LightstreamerClient as LightstreamerClient.connectionOptions.
+     */
+    class ConnectionOptions {
+        std::unique_ptr<session::InternalConnectionOptions> internal;
+        std::mutex mtx;
+
+    public:
+        explicit ConnectionOptions(std::unique_ptr<session::InternalConnectionOptions> internal)
+                : internal(std::move(internal)) {}
+
+        /**
+         * @brief Gets or sets the ConnectTimeout property.
+         *
+         * ConnectTimeout is deprecated, please use RetryDelay instead.
+         *
+         * @throws std::invalid_argument If the provided value is not valid.
+         */
+        void setConnectTimeout(const std::string& value) {
+            std::lock_guard<std::mutex> lock(mtx);
+            if (value == "AUTO") {
+                return; // use default value of retryDelay
+            }
+            try {
+                long n = std::stol(value);
+                internal->setRetryDelay(n);
+            } catch (const std::invalid_argument&) {
+                throw std::invalid_argument("The given value is not a valid value for setConnectTimeout. Use a positive number or the string \"auto\"");
+            }
+        }
+
+        /**
+         * @brief Gets the current connection timeout.
+         *
+         * This property is read-only.
+         */
+        std::string getConnectTimeout()  {
+            std::lock_guard<std::mutex> lock(mtx);
+            return std::to_string(internal->getRetryDelay());
+        }
+
+        /**
+         * @brief Gets the current connection timeout.
+         *
+         * This is the extra time that the system may wait for a response before deciding to drop the
+         * connection and try another approach. This timeout is managed internally and may vary based
+         * on network conditions.
+         *
+         * @return The current connect timeout in milliseconds.
+         */
+        long getCurrentConnectTimeout() const {
+            std::lock_guard<std::mutex> lock(mtx);
+            return internal->getCurrentConnectTimeout();
+        }
+
+        /**
+         * @brief Gets or sets the ContentLength.
+         */
+        long getContentLength() const {
+            std::lock_guard<std::mutex> lock(mtx);
+            return internal->getContentLength();
+        }
+
+        void setContentLength(long value) {
+            std::lock_guard<std::mutex> lock(mtx);
+            internal->setContentLength(value);
+        }
+
+        /**
+         * @brief Gets or sets the FirstRetryMaxDelay.
+         */
+        long getFirstRetryMaxDelay() const {
+            std::lock_guard<std::mutex> lock(mtx);
+            return internal->getFirstRetryMaxDelay();
+        }
+
+        void setFirstRetryMaxDelay(long value) {
+            std::lock_guard<std::mutex> lock(mtx);
+            internal->setFirstRetryMaxDelay(value);
+        }
+
+        /**
+         * @brief Gets or sets the ForcedTransport.
+         */
+        std::string getForcedTransport() const {
+            std::lock_guard<std::mutex> lock(mtx);
+            return internal->getForcedTransport();
+        }
+
+        void setForcedTransport(const std::string& value) {
+            std::lock_guard<std::mutex> lock(mtx);
+            internal->setForcedTransport(value);
+        }
+
+        /**
+         * @brief Gets or sets HTTP extra headers.
+         */
+        std::map<std::string, std::string> getHttpExtraHeaders() const {
+            std::lock_guard<std::mutex> lock(mtx);
+            return internal->getHttpExtraHeaders();
+        }
+
+        void setHttpExtraHeaders(const std::map<std::string, std::string>& headers) {
+            std::lock_guard<std::mutex> lock(mtx);
+            internal->setHttpExtraHeaders(headers);
+        }
+
+
+
+    };
+
+}
 
 #endif //LIGHTSTREAMER_LIB_CLIENT_CPP_CONNECTIONOPTIONS_HPP

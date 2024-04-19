159bda1595a6e3e92dbd13286f9e4f356ace0988
Joaquin Bejar <jb@taunais.com>
2024-04-19 08:52:41 +0200

    v0.1.0.alpha.4 update Protocol

--- include/lightstreamer/client/protocol/Protocol.hpp
+++ include/lightstreamer/client/protocol/Protocol.hpp
@@ -26,19 +26,19 @@
 
 #include <memory>
 #include <lightstreamer/client/protocol/ProtocolListener.hpp>
-#include "ForceRebindRequest.h"
-#include "DestroyRequest.h"
-#include "ConstrainRequest.h"
-#include "CreateSessionRequest.h"
-#include "BindSessionRequest.h"
-#include "MessageRequest.h"
-#include "SubscribeRequest.h"
-#include "UnsubscribeRequest.h"
-#include "ChangeSubscriptionRequest.h"
-#include "ReverseHeartbeatRequest.h"
-#include "RequestManager.h"
+#include <lightstreamer/client/requests/ForceRebindRequest.hpp>
+#include <lightstreamer/client/requests/DestroyRequest.hpp>
+#include <lightstreamer/client/requests/ConstrainRequest.hpp>
+#include <lightstreamer/client/requests/CreateSessionRequest.hpp>
+#include <lightstreamer/client/requests/BindSessionRequest.hpp>
+#include <lightstreamer/client/requests/MessageRequest.hpp>
+#include <lightstreamer/client/requests/SubscribeRequest.hpp>
+#include <lightstreamer/client/requests/UnsubscribeRequest.hpp>
+#include <lightstreamer/client/requests/ChangeSubscriptionRequest.hpp>
+#include <lightstreamer/client/requests/ReverseHeartbeatRequest.hpp>
+#include <lightstreamer/client/protocol/RequestManager.hpp>
 #include <lightstreamer/client/requests/RequestTutor.hpp>
-#include "ConstrainTutor.hpp"
+#include <lightstreamer/client/session/Session.hpp>
 #include <lightstreamer/util/ListenableFuture.hpp>
 #include <string>
 #include <exception>
@@ -66,27 +66,27 @@ namespace lightstreamer::client::protocol {
 
         virtual void setListener(std::shared_ptr<ProtocolListener> listener) = 0;
 
-        virtual void sendForceRebind(const ForceRebindRequest& request, const std::shared_ptr<requests::RequestTutor>& tutor) = 0;
+        virtual void sendForceRebind(const requests::ForceRebindRequest& request, const std::shared_ptr<requests::RequestTutor>& tutor) = 0;
 
-        virtual void sendDestroy(const DestroyRequest& request, const std::shared_ptr<requests::RequestTutor>& tutor) = 0;
+        virtual void sendDestroy(const requests::DestroyRequest& request, const std::shared_ptr<requests::RequestTutor>& tutor) = 0;
 
-        virtual void sendConstrainRequest(const ConstrainRequest& request, const std::shared_ptr<ConstrainTutor>& tutor) = 0;
+        virtual void sendConstrainRequest(const requests::ConstrainRequest& request, const std::shared_ptr<session::Session::ConstrainTutor>& tutor) = 0;
 
-        virtual void sendCreateRequest(const CreateSessionRequest& request) = 0;
+        virtual void sendCreateRequest(const requests::CreateSessionRequest& request) = 0;
 
-        virtual std::shared_ptr<util::ListenableFuture> sendBindRequest(const BindSessionRequest& request) = 0;
+        virtual std::shared_ptr<util::ListenableFuture> sendBindRequest(const requests::BindSessionRequest& request) = 0;
 
         virtual void stop(bool waitPendingControlRequests, bool forceConnectionClose) = 0;
 
-        virtual void sendMessageRequest(const MessageRequest& request, const std::shared_ptr<requests::RequestTutor>& tutor) = 0;
+        virtual void sendMessageRequest(const requests::MessageRequest& request, const std::shared_ptr<requests::RequestTutor>& tutor) = 0;
 
-        virtual void sendSubscriptionRequest(const SubscribeRequest& request, const std::shared_ptr<requests::RequestTutor>& tutor) = 0;
+        virtual void sendSubscriptionRequest(const requests::SubscribeRequest& request, const std::shared_ptr<requests::RequestTutor>& tutor) = 0;
 
-        virtual void sendUnsubscriptionRequest(const UnsubscribeRequest& request, const std::shared_ptr<requests::RequestTutor>& tutor) = 0;
+        virtual void sendUnsubscriptionRequest(const requests::UnsubscribeRequest& request, const std::shared_ptr<requests::RequestTutor>& tutor) = 0;
 
-        virtual void sendConfigurationRequest(const ChangeSubscriptionRequest& request, const std::shared_ptr<requests::RequestTutor>& tutor) = 0;
+        virtual void sendConfigurationRequest(const requests::ChangeSubscriptionRequest& request, const std::shared_ptr<requests::RequestTutor>& tutor) = 0;
 
-        virtual void sendReverseHeartbeat(const ReverseHeartbeatRequest& request, const std::shared_ptr<requests::RequestTutor>& tutor) = 0;
+        virtual void sendReverseHeartbeat(const requests::ReverseHeartbeatRequest& request, const std::shared_ptr<requests::RequestTutor>& tutor) = 0;
 
         virtual void copyPendingRequests(std::shared_ptr<Protocol> protocol) = 0;
 
@@ -100,10 +100,10 @@ namespace lightstreamer::client::protocol {
 
         /// Opens a WebSocket connection. If a connection is already open (this can happen when the flag isEarlyWSOpenEnabled is set),
         /// the connection is closed and a new connection is opened.
-        virtual std::shared_ptr<ListenableFuture> openWebSocketConnection(const std::string& serverAddress) = 0;
+        virtual std::shared_ptr<util::ListenableFuture> openWebSocketConnection(const std::string& serverAddress) = 0;
 
         /// Forward the session recovery request to the transport layer.
-        virtual void sendRecoveryRequest(const RecoverSessionRequest& request) = 0;
+        virtual void sendRecoveryRequest(const requests::CreateSessionRequest& request) = 0;
 
         /// Set the default sessionId so the protocol can omit parameter LS_session from requests.
         virtual void setDefaultSessionId(const std::string& sessionId) = 0;
@@ -112,8 +112,6 @@ namespace lightstreamer::client::protocol {
         /// It is the value of the parameter LS_inactivity_millis sent with a bind_session request.
         /// It doesn't change during the life of a session.
         virtual long getMaxReverseHeartbeatIntervalMs() const = 0;
-
-        virtual void sendRecoveryRequest(const RecoverSessionRequest& request) = 0;
 
         virtual void stopActive(bool forceConnectionClose) = 0;
     };
